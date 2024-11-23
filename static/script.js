44bddbb61b4b0e655dce2bbefb464c07614ea82d
Ailya-Shah <rhooke44@gmail.com>
2024-11-23 21:03:51 +0500

    added javascript

---
+++ static/script.js
@@ -0,0 +1,7 @@
+// Example function to show an alert when the button is clicked
+function showAlert() {
+    alert('Welcome to German Counting!');
+  }
+  
+  // You can also create more complex interactions as needed.
+  
