d5a3ce775123acd3af2c9394cfe97248fb5fabfd
Ailya-Shah <rhooke44@gmail.com>
2024-11-24 00:46:20 +0500

    adding api

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# Define the route for the home page
 @app.route('/')
-def index():
+def home():
+    # Render the index.html page
     return render_template('index.html')
 
-@app.route('/flashcard1')
-def flashcard1():
-    return render_template('flashcard1.html')
+# Define the route for translation
+@app.route('/translate', methods=['POST'])
+def translate():
+    # Get the text input from the form in index.html
+    text_to_translate = request.form.get('text_to_translate')
 
-@app.route('/flashcard2')
-def flashcard2():
-    return render_template('flashcard2.html')
+    # Set up the MyMemory API URL
+    api_url = 'https://api.mymemory.translated.net/get'
+
+    # Prepare the parameters for the API request
+    params = {
+        'q': text_to_translate,  # The text to translate
+        'langpair': 'en|de'      # Source language (English) and target language (German)
+    }
+
+    # Send the API request
+    response = requests.get(api_url, params=params)
+
+    if response.status_code == 200:
+        # Extract the translation from the API response
+        translation = response.json()['responseData']['translatedText']
+    else:
+        translation = "Error: Unable to fetch translation"
+
+    # Render the results in the same template (or a new one)
+    return render_template('index.html', translation=translation)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
